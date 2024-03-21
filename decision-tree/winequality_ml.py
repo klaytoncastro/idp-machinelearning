6b69909b769da5b548271202b2eff45bb6e2817e
Klayton Rodrigues de Castro <klayton.castro@gmail.com>
2024-03-21 05:35:27 -0300

    Update ML Metrics

--- decision-tree/winequality_ml.py
+++ decision-tree/winequality_ml.py
@@ -1,6 +1,5 @@
 # Importando o dataset
 import pandas as pd
-
 url = 'https://raw.githubusercontent.com/klaytoncastro/idp-machinelearning/main/decision-tree/winequality-merged.csv'
 arquivo = pd.read_csv(url)
 
@@ -8,16 +7,16 @@ arquivo = pd.read_csv(url)
 # print(arquivo.head())
 arquivo.head()
 
-# Verificando a coluna 'color' 
+# Verificando a coluna 'color' e convertendo os dados categóricos para um formato numérico. 
 if 'color' in arquivo.columns:
     print("Column 'color' exists in the DataFrame.")
-    # Replace 'red' with 0 and 'white' with 1
+    # Substituindo valores correspondentes a 'red' por 0, e 'white' por 1
     arquivo['color'] = arquivo['color'].replace('red', 0)
     arquivo['color'] = arquivo['color'].replace('white', 1)
 else:
     print("Column 'color' does not exist in the DataFrame.")
 
-# Dividindo dados para treino e teste.
+# Dividindo os conjuntos de dados para treino e teste.
 y = arquivo['color']
 X = arquivo.drop('color', axis = 1)
 
@@ -25,7 +24,7 @@ X = arquivo.drop('color', axis = 1)
 from sklearn.model_selection import train_test_split
 
 # Considerando x o conjunto de variáveis, y como variável alvo do modelo e definindo o tamanho do conjunto de teste.  
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.7)
 
 # Importando o Algoritmo de Aprendizagem Supervisionada para criação da Árvore de Decisão e treinando o modelo.
 from sklearn.ensemble import ExtraTreesClassifier
