8e9c8574cbaa3ef42599253cca6dd7eb05df77df
Klayton Rodrigues de Castro <klayton.castro@gmail.com>
2024-03-28 12:09:14 -0300

    Update Classifier / Regressor

---
+++ decision-tree/winequality_ml_classifier.py
@@ -0,0 +1,112 @@
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/klaytoncastro/idp-machinelearning/main/decision-tree/winequality-merged.csv'
+arquivo = pd.read_csv(url)
+arquivo.head()
+
+# Informações gerais sobre o dataset
+print(arquivo.info())
+
+# Descrição estatística das variáveis numéricas
+print(arquivo.describe())
+
+print(arquivo['color'].value_counts())
+
+import matplotlib.pyplot as plt
+arquivo.hist(figsize=(20, 15), bins=20)
+plt.show()
+
+arquivo.boxplot(figsize=(20, 10), rot=90)
+plt.show()
+
+# Check if 'color column exists in the DataFrame
+if 'color' in arquivo.columns:
+    print("Column 'color' exists in the DataFrame.")
+    # Replace 'red' with 0 and 'white' with 1
+    arquivo['color'] = arquivo['color'].replace('red', 0)
+    arquivo['color'] = arquivo['color'].replace('white', 1)
+else:
+    print("Column 'color' does not exist in the DataFrame.")
+
+# Display the first few rows of the DataFrame
+print(arquivo.head())
+
+y = arquivo['quality']
+X = arquivo.drop('quality', axis = 1)
+
+from sklearn.model_selection import train_test_split
+
+# Assuming x is your feature set and y is the target variable
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+from sklearn.ensemble import ExtraTreesRegressor
+modelo = ExtraTreesRegressor()
+modelo.fit(x_train, y_train)
+
+resultado = modelo.score(x_test, y_test)
+print ("Acurácia:", resultado)
+
+# Adicionando a verificação da Precisão, Recall e F1-Score
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+# Fazendo previsões no conjunto de teste
+y_pred = modelo.predict(x_test)
+
+# Calculando as métricas
+precisao = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+print("Precisão:", precisao)
+print("Recall:", recall)
+print("F1-score:", f1)
+
+from sklearn.metrics import confusion_matrix, classification_report
+
+"""
+Calculando e exibindo a matriz de confusão. A orientação padrão é a seguinte:
+[0,0]: Verdadeiros Negativos (VN) - Previsões corretamente identificadas como negativas.
+[0,1]: Falsos Positivos (FP) - Previsões incorretamente identificadas como positivas.
+[1,0]: Falsos Negativos (FN) - Previsões incorretamente identificadas como negativas.
+[1,1]: Verdadeiros Positivos (VP) - Previsões corretamente identificadas como positivas.
+"""
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("Matriz de Confusão:")
+print(conf_matrix)
+
+"""
+Calculando e exibindo as métricas de classificação.
+Se algumas classes têm muito mais amostras do que outras, isso pode influenciar o desempenho e confiabilidade do modelo.
+
+O "support" refere-se à quantidade de ocorrências da classe específica no conjunto de dados, sendo útil para verificar desbalanceamentos.
+A "macro avg" calcula a média aritmética das métricas (precisão, recall, F1-score) para cada classe, sem considerar o número de instâncias em cada classe (support).
+A "weighted avg" calcula a média ponderada das métricas para cada classe, considerando o número de instâncias em cada classe (support).
+"""
+print("Relatório de Classsificação:")
+print(classification_report(y_test, y_pred))
+
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+
+# Prever as probabilidades para os dados de teste.
+# Note que estamos interessados nas probabilidades da classe positiva (1), então usamos [:, 1].
+y_probs = modelo.predict_proba(x_test)[:, 1]
+
+# Calcular FPR, TPR, e limiares
+fpr, tpr, thresholds = roc_curve(y_test, y_probs)
+
+# Calcular a AUC
+roc_auc = auc(fpr, tpr)
+
+# Plotar a curva ROC
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
---
+++ decision-tree/winequality_ml_regressor.py
@@ -0,0 +1,149 @@
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/klaytoncastro/idp-machinelearning/main/decision-tree/winequality-merged.csv'
+arquivo = pd.read_csv(url)
+arquivo.head()
+
+# Informações gerais sobre o dataset
+print(arquivo.info())
+
+# Descrição estatística das variáveis numéricas
+print(arquivo.describe())
+
+print(arquivo['color'].value_counts())
+
+import matplotlib.pyplot as plt
+arquivo.hist(figsize=(20, 15), bins=20)
+plt.show()
+
+arquivo.boxplot(figsize=(20, 10), rot=90)
+plt.show()
+
+# Check if 'color column exists in the DataFrame
+if 'color' in arquivo.columns:
+    print("Column 'color' exists in the DataFrame.")
+    # Replace 'red' with 0 and 'white' with 1
+    arquivo['color'] = arquivo['color'].replace('red', 0)
+    arquivo['color'] = arquivo['color'].replace('white', 1)
+else:
+    print("Column 'color' does not exist in the DataFrame.")
+
+# Display the first few rows of the DataFrame
+print(arquivo.head())
+
+#y = arquivo['color']
+#X = arquivo.drop('color', axis = 1)
+
+y = arquivo['quality']
+X = arquivo.drop('quality', axis = 1)
+
+from sklearn.model_selection import train_test_split
+
+# Assuming x is your feature set and y is the target variable
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import RFE
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+# Importando StandardScaler
+from sklearn.preprocessing import StandardScaler
+
+# Escalamento dos dados
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train)
+x_test_scaled = scaler.transform(x_test)
+
+# RFE
+modelo_rfe = ExtraTreesRegressor()
+rfe = RFE(estimator=modelo_rfe, n_features_to_select=10)  # Ajustar conforme necessário
+x_train_rfe = rfe.fit_transform(x_train_scaled, y_train)
+x_test_rfe = rfe.transform(x_test_scaled)
+
+# PCA
+pca = PCA(n_components=0.95)  # Mantém 95% da variancia
+x_train_pca = pca.fit_transform(x_train_rfe)
+x_test_pca = pca.transform(x_test_rfe)
+
+#from sklearn.ensemble import ExtraTreesClassifier
+#modelo = ExtraTreesClassifier()
+
+from sklearn.ensemble import ExtraTreesRegressor
+modelo = ExtraTreesRegressor()
+
+modelo.fit(x_train, y_train)
+
+resultado = modelo.score(x_test, y_test)
+print ("Acurácia:", resultado)
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
+
+# Realizar previsões com o modelo
+y_pred = modelo.predict(x_test)  # Supondo que este seja o passo de previsão no seu fluxo
+
+# Arredondamento das previsões para o inteiro mais próximo
+y_pred_arredondado = np.round(y_pred)
+
+# Ajuste dos valores previstos arredondados para garantir que estejam dentro da escala de 1 a 10
+y_pred_ajustado = np.clip(y_pred_arredondado, 1, 10)
+
+# Cálculo das métricas usando os valores previstos ajustados
+mse = mean_squared_error(y_test, y_pred_ajustado)
+rmse = np.sqrt(mse)
+mae = mean_absolute_error(y_test, y_pred_ajustado)
+r2 = r2_score(y_test, y_pred_ajustado)
+
+# Imprimindo as métricas ajustadas
+print("MSE ajustado:", mse)
+print("RMSE ajustado:", rmse)
+print("MAE ajustado:", mae)
+print("R² ajustado:", r2)
+
+# Calculando a diferença absoluta entre as previsões ajustadas e os valores reais
+diferencas_absolutas = np.abs(y_pred_ajustado - y_test)
+
+# Definindo um critério de acurácia (previsões dentro de ±1 unidade do valor real)
+criterio_acuracia = 1
+
+# Calculando a proporção de previsões que satisfazem o critério
+acuracia_ajustada = np.mean(diferencas_absolutas <= criterio_acuracia)
+
+# Exibindo a acurácia ajustada
+print("Acurácia ajustada (proporção de previsões dentro de ±1 da real):", acuracia_ajustada)
+
+resultado = modelo.score(x_test, y_test)
+print ("Acurácia:", resultado)
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+# Supondo que y_pred seja as suas previsões e y_test os valores reais
+
+# Calculando as métricas
+mse = mean_squared_error(y_test, y_pred)
+rmse = mse ** 0.5
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+# Imprimindo as métricas
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("MAE:", mae)
+
+"""
+MSE : 1.2343589743589745
+RMSE : 1.1110170900391112
+MAE : 0.8261538461538461
+R² : -0.6161209584924814
+-----
+MSE ajustado: 0.4323076923076923
+RMSE ajustado: 0.6575010968110184
+MAE ajustado: 0.35025641025641024
+R² ajustado: 0.453820531776159
+
+Acurácia ajustada
+(proporção de previsões dentro de ±1 da real):
+0.9661538461538461
+-----
+"""
